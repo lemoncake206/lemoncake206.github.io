62f9b07be742196334ae968670c13342e572d2d4
lemoncake206 <144384708+lemoncake206@users.noreply.github.com>
2023-09-09 17:46:16 -0400

    Update writing_program.py

--- writing_program.py
+++ writing_program.py
@@ -1,9 +1,38 @@
-file = open('writing_storage.txt','r')
-current = file.readlines().strip()
-for x in current:
-    print(x)
+def get_file():
+    file = open('writing_storage.txt', 'r')
+    line = file.readline().strip()
+    lines = []
+    count = 0
 
-def add_value(name: str, amount: int):
-    if name == 'Kate':
-        x = 0
+    while line != '':
+        if count % 2 == 0:
+            lines.append(line)
+        else:
+            lines.append(int(line))
+        count += 1
+        line = file.readline().strip()
+    file.close()
+    return lines
+        
+def rewrite_file(lines):
+    file = open('writing_storage.txt', 'w')
+    for line in lines:
+        file.write(str(line))
+        file.write('\n')
+    file.close()
     
+    
+def add_value(name: str, amount: int):
+    lines = get_file()
+    for x in range(len(lines)):
+        if lines[x] == name:
+            lines[x+1] += amount
+    rewrite_file(lines)
+            
+
+def subtract_value(name: str, amount: int):
+    lines = get_file()
+    for x in range(len(lines)):
+        if lines[x] == name:
+            lines[x+1] -= amount
+    rewrite_file(lines)
